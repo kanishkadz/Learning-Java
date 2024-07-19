85473360e59a7ddac04b79437d877d1d4cd36048
Kanishka Anand <kanishkaadz@gmail.com>
2024-07-19 19:57:21 +0530

    Update Arev.java

--- Arev.java
+++ Arev.java
@@ -3,6 +3,32 @@ class Arev{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size:");
-        
+        int n = sc.nextInt();
+        int A[] = new int[n];
+        int B[] = new int[n];
+        int i=0, j, rev, r, no;
+        System.out.println("Enter "+n+" elements:");
+        while(i<n){
+            no = sc.nextInt();
+            if(no>9){
+                A[i] = no;
+                i++;
+            } 
+            else {
+                System.out.println("Value must be greater than 9");
+            }
+        }
+        for(i=0; i<n; i++){
+            rev = 0;
+            for(j=A[i]; j>0; j/=10){
+                r = j%10;
+                rev = rev*10+r; 
+            }
+            B[i] = rev;
+        }
+        System.out.println("REsultant Array is :");
+        for(i=0; i<n; i++){
+            System.out.println(B[i]);
+        }
     }
 }
