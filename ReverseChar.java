1a7756ecf20b41df0907df269d32908b16574b46
Kanishka Anand <kanishkaadz@gmail.com>
2024-08-19 21:39:52 +0530

    2

--- ReverseChar.java
+++ ReverseChar.java
@@ -1,11 +1,14 @@
-import java.io.*;
-class ReverseChar{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+class ReverseChar {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter character:");
-        char c = sc.next().charAt(0);
-        int len = c.length();
-        for(int i=len-1; i>=0; i--)
-            System.out.print(c.charAt(i));
+        System.out.print("Enter character: ");
+        String input = sc.next(); 
+        int len = input.length(); 
+        for (int i = len - 1; i >= 0; i--) {
+            System.out.print(input.charAt(i)); 
+        }
+        sc.close(); 
     }
 }
