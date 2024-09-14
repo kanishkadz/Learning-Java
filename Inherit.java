e430b6925cc84cd74e785ee7fdcca03ceb0d3cbf
Kanishka Anand <kanishkaadz@gmail.com>
2024-09-14 15:47:32 +0530

    Update Inherit.java

--- Inherit.java
+++ Inherit.java
@@ -1,41 +1,49 @@
 import java.util.Scanner;
-class Emp{
+
+class Emp {
     int age, salary;
     String name;
-    public void getEmp(){
+
+    public void getEmp() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter name:");
+        System.out.print("Enter name: ");
         name = sc.nextLine();
-        System.out.print("Enter age:");
+        System.out.print("Enter age: ");
         age = sc.nextInt();
-        System.out.print("Enter salary:");
+        System.out.print("Enter salary: ");
         salary = sc.nextInt();
+        sc.nextLine();
     }
-    public void showEmp(){
-        System.out.println("Name = "+name);
-        System.out.println("Age = "+age);
-        System.out.println("Salary = "+salary);
+
+    public void showEmp() {
+        System.out.println("Name = " + name);
+        System.out.println("Age = " + age);
+        System.out.println("Salary = " + salary);
     }
 }
-class Dept extends Emp{
+
+class Dept extends Emp {
     String des;
     char grade;
     int basic;
-    public void getDept(){
+
+    public void getDept() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter destination:");
+        System.out.print("Enter designation: ");
         des = sc.nextLine();
-        System.out.print("Enter grade:");
-        grade = sc.charAt(0);     
-        System.out.println("Enter salary:");
-        basic = sc.nextInt();   
+        System.out.print("Enter grade: ");
+        grade = sc.next().charAt(0);
+        System.out.print("Enter basic salary: ");
+        basic = sc.nextInt();
     }
-    public void showDept(){
-        System.out.println("Designation = "+des);
-        System.out.println("Grade = "+grade);
-        System.out.println("Basic Salary = "+basic);
+
+    public void showDept() {
+        System.out.println("Designation = " + des);
+        System.out.println("Grade = " + grade);
+        System.out.println("Basic Salary = " + basic);
     }
 }
+
 public class Inherit {
     public static void main(String[] args) {
         Dept ob = new Dept();
