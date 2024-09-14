68cbaa537b2990308fac3e599a9ecf81b856e9ec
Kanishka Anand <kanishkaadz@gmail.com>
2024-09-14 12:30:48 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,41 +1,42 @@
 import java.util.Scanner;
-class Employee
-{
-	String name;
-	String deg;
-	double bs,da,pf,gs,ns;
-	public void getEmp()
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter Name:");
-		name=sc.nextLine();
-		System.out.print("Enter Degisnation:");
-		deg=sc.nextLine();
-		System.out.print("Enter Basic Salary:");
-		bs=sc.nextDouble();
-	} 
 
-	public void calSalary()
-	{
-		da=(bs*45)/100;
-		pf=(bs*12.5)/100;
-		gs=bs+da;
-		ns=bs-pf;
-	}
-	public void showEmp(){
-		System.out.println("Name:"+name);
-		System.out.println("Desig:"+deg);
-		System.out.println("Basic:"+bs);
-		System.out.println("DA:"+da);
-		System.out.println("PF:"+pf);
-		System.out.println("GS:"+gs);
-		System.out.println("NS:"+ns);	
-	}
-	public static void main(String arg[])
-	{
-		Employee ob=new Employee();	//Object creation statement
-		ob.getEmp();
-		ob.calSalary();
-		ob.showEmp();
-	}
+class Employee {
+    String name;
+    String designation;
+    int basic;
+    float gs;
+    float da;
+    float pf;
+    float ns;
+
+    public void getEmp() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        name = sc.nextLine();
+        System.out.print("Enter designation: ");
+        designation = sc.nextLine();
+        System.out.print("Enter basic salary: ");
+        basic = sc.nextInt();
+        sc.close();
+    }
+
+    public void calSal() {
+        da = basic * 0.45f;
+        pf = basic * 0.125f;
+        gs = basic + da;
+        ns = gs - pf;
+    }
+
+    public void showEmp() {
+        System.out.println("Name = " + name);
+        System.out.println("Designation = " + designation);
+        System.out.println("Net Salary = " + ns);
+    }
+
+    public static void main(String args[]) {
+        Employee ob = new Employee();
+        ob.getEmp();
+        ob.calSal();
+        ob.showEmp();
+    }
 }
