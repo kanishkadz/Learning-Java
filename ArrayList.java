e1074629c7e5780d06abe59799c0c4bd4ea1bf4a
Kanishka Anand <kanishkaadz@gmail.com>
2024-11-24 12:06:49 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ArrayList {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
+    
+}
